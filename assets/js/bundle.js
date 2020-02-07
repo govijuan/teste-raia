f458f3b095ec2d40e6145004435366c82a23cbe1
Juan Carlos Coral <govindasd@gmail.com>
2020-02-07 09:16:19 -0300

    Completos estilos e funcionalidades
    Inclui centrar o Login quando estiver ativo.
    
    Não foram verificados estilos para login responsivo ainda.

--- assets/js/bundle.js
+++ assets/js/bundle.js
@@ -6,7 +6,6 @@
     const create = element => document.createElement(element);
 
     const app = selector('#app');
-
     const Login = create('div');
     Login.classList.add('login');
 
@@ -35,13 +34,17 @@
             : button.removeAttribute('disabled');
     };
 
-    Form.innerHTML = '<div class="form-group"><input type="email" name="email" id="email" /></div><div class="form-group"><input type="password" name="password" id="password" /></div><input type="submit" value="Entrar" />';
+    Form.innerHTML = `  
+                        <input type="email" name="email" id="email" placeholder="Entre com seu e-mail"/>
+                        <input type="password" name="password" id="password" placeholder="Digite sua senha supersecreta"/>
+                        <input name="button" type="submit" value="Entrar" disabled/> 
+                    `;
     
     const mainURL = 'http://www.mocky.io/v2/5dba690e3000008c00028eb6';
 
     app.appendChild(Logo);
     Login.appendChild(Form);
-
+    
     async function fakeAuthenticate(email, password) {
         
         let headers = new Headers();
@@ -68,32 +71,31 @@
     }
 
     async function getDevelopersList(url) {
-        /**
-         * bloco de código omitido
-         * aqui esperamos que você faça a segunda requisição 
-         * para carregar a lista de desenvolvedores
-         */
         return fetch(url).then(res => res.json())
     }
 
     function renderPageUsers(users) {
+
         app.classList.add('logged');
-        //Login.style.display = /* trecho omitido */
+        if(document.getElementsByClassName('login').length > 0){
+            Login.style.height = '0';
+            setTimeout(()=>{Login.style.display = 'none'}, 5000);
+        }
+        
         Login.style.display = 'none';
         const Ul = create('ul');
         Ul.classList.add('container')
 
-        /**
-         * bloco de código omitido
-         * exiba a lista de desenvolvedores
-         */
         users.map((user, key) => {
-            const userElement = `<li id=${user.id} class="user-container">
-                                    <div class="avatar-cont">
-                                        <img src=${user.avatar_url} alt="${user.login}'s avatar" />
-                                    </div>
-                                    <div class="user-name">${user.login}</div>
-                                </li>`;
+            const userElement = `   <li id=${user.id} class="user-container">
+                                        <a href=${user.html_url} target="_blank">
+                                            <div class="avatar-cont">
+                                                <img src=${user.avatar_url} alt="${user.login}'s avatar" />
+                                            </div>
+                                            <div class="user-name">${user.login}</div>
+                                        </a>
+                                    </li>
+                                `;
             Ul.insertAdjacentHTML('beforeend', userElement);
         })
 
@@ -102,17 +104,20 @@
 
     // init
     (async function(){
-        console.log('Rodando init');
         const rawToken = sessionStorage.getItem('token');
         const token = rawToken ? rawToken.split('.') : null
         if (!token || token[2] < (new Date()).getTime()) {
             localStorage.removeItem('token');
             location.href='#login';
             app.appendChild(Login);
+            const appHeight = app.offsetHeight;
+            const loginPadding = ( appHeight / 2 ) -116;
+            app.style.paddingTop = `${loginPadding}px`;
         } else {
             location.href='#users';
             const users = await getDevelopersList(atob(token[1]));
             renderPageUsers(users);
+            app.style.paddingTop = '';
         }
     })()
 })()
